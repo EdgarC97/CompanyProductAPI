97e8f695020f5963e3bc730a18863dc4b028aa58
EdgarC97 <ecardona742@gmail.com>
2025-05-03 09:42:51 -0500

    feat: create models Company and Product

---
+++ Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace CompanyProductAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int CompanyId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public Company Company { get; set; }
+    }
+}
