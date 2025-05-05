b1e9c47869c5ff2c24b2b244480325d9ab068f14
EdgarC97 <ecardona742@gmail.com>
2025-05-05 15:00:59 -0500

    feat: add .env.example

--- Data/DbConnectionFactory.cs
+++ Data/DbConnectionFactory.cs
@@ -5,16 +5,9 @@ namespace CompanyProductAPI.Data
 {
     public class DbConnectionFactory
     {
-        private readonly IConfiguration _configuration;
-
-        public DbConnectionFactory(IConfiguration configuration)
-        {
-            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
-        }
-
         public IDbConnection CreateConnection()
         {
-            var connectionString = _configuration.GetConnectionString("DB_CONNECTION_STRING");
+            var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
             if (string.IsNullOrEmpty(connectionString))
             {
                 throw new InvalidOperationException("DB_CONNECTION_STRING not found in environment variables.");
